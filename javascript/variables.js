e5f0098dfabfb4e5b869bdb142df31ad2b7a7d48
Faduma Omar <fadumaomar63@gmail.com>
2024-02-26 22:19:45 +0000

    I added string template

---
+++ javascript/variables.js
@@ -0,0 +1,63 @@
+
+firstName= "Faduma" 
+
+lastName = "Farah"
+
+//console.log(firstName + lastName);
+
+
+//let address = '10 Downing Street'
+
+
+//let address = new String("10 Downing Street");
+
+//console.log(address.toString());
+
+//let city = 'London'
+
+//let address= new String(`10 downing street, ${city}, W10`);
+
+//console.log(address.toString());
+
+let city = 'Bristol'
+
+let address = new String(`15 bath road, ${city}, UK`);
+
+//console.log(address.toString());
+
+//console.log(address.toLowerCase());git
+
+//console.log(address.toUpperCase())
+
+console.log(address.replace('Bristol', 'Kent'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
